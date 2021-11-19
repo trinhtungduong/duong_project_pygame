fd943bce4acd647c11e73f869ec3ebf71c1dfa9d
trinhtungduong <sync20032001@gmail.com>
2021-11-19 12:12:02 +0700

    first commit

---
+++ step2/main.py
@@ -0,0 +1,26 @@
+import pygame,sys
+from setting import *
+from level import Level
+from game_data import level_0
+
+pygame.init()
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+clock = pygame.time.Clock()
+level = Level(level_0,screen)
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    screen.fill('black')
+    level.run()
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
+
+
